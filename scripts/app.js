8a1c35fff2f2d4f4602a00cacbc3567e2f2295f2
Joshua King <joshuaking85@gmail.com>
2016-10-28 19:49:16 -0400

    Adding the ng-repeat directive to loop through all of the TODO list items

--- scripts/app.js
+++ scripts/app.js
@@ -2,22 +2,15 @@ angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope) {
    $scope.helloWorld = function() {
        console.log("hello there, this is the mainCtrl Function!");
-   }; 
-})
+};
 
-.controller('coolCtrl', function($scope) {
-   $scope.whoAmI = function() {
-       console.log("hello there, this is the coolCtrl Function!");
-   };
-    
-    $scope.helloWorld = function() {
-        console.log("This is not the main ctrl!");
-    }
-    
-})
-
-.controller('imASibling', function($scope) {
-   $scope.foobar = 1234; 
-    
-    // Do other cool stuff
+   $scope.todos = [
+            {"name": "clean the house"},
+            {"name": "water the dog"},
+            {"name": "feed the lawn"},
+            {"name": "pay dem bills"},
+            {"name": "run"},
+            {"name": "swim"}
+            ]
+            
 });
